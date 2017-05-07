a6e279e1071a8c8ca8143864f3dc2a2430c9cf54
dangnabit <dan.gabel@gmail.com>
2017-05-06 22:55:32 -0500

    Initial Commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Gut Buster');
+INSERT INTO burgers (burger_name) VALUES ('Juicy Lucy');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Stack', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Jane', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('BBQ Dream');
