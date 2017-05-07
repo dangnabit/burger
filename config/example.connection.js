5a266466f5e7e5d4c766acb176c391e3b4130d85
dangnabit <dan.gabel@gmail.com>
2017-05-06 23:06:53 -0500

    Added JawsDB connection to example config

--- config/example.connection.js
+++ config/example.connection.js
@@ -1,18 +1,22 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: '<password>',
-  database: 'burger_db'
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '<password>',
+        database: 'burger_db'
+    });
+}
 connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
 });
 
 module.exports = connection;
