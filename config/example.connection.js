fb408b9709e23be82cd9fb7e9b42a8b5e802a322
dangnabit <dan.gabel@gmail.com>
2017-05-12 22:50:27 -0500

    fixed port in server

--- config/example.connection.js
+++ config/example.connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     port: 3306,
-    host: 'gk90usy5ik2otcvi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'remhzug258wa58lq',
-    password: 'i17npsx13p6gaqlq',
-    database: 'mlsdxu3q18uxt1yk'
+    host: 'localhost',
+    user: '<username>',
+    password: '<password>',
+    database: 'burgers_db'
   });
 }
 connection.connect(function(err) {
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000;
+
 
 var app = express();
+
+app.set('port', (process.env.PORT || 3000))
 
 app.use(express.static(process.cwd() + "/public"));
 
@@ -22,6 +24,6 @@ var routes = require("./controllers/burger_controller.js");
 
 app.use("/", routes);
 
-app.listen(port, function() {
+app.listen(app.get('port'), function() {
 	console.log('Server listening on port: ' + port);
 });
